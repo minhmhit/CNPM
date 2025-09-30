2bf367a5942e89ea766154f3618c6b8e75fe687e
Neo Bam <nam@example.com>
2025-09-30 12:59:04 +0700

    basic layout v1

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./Home.jsx";
+import Login from "./Login.jsx";
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  );
+}
---
+++ Frontend/src/Home.jsx
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom";
+import background from "./pics/backgroud.png";
+import logo from "./pics/logo.png";
+import "./App.css";
+export default function Home() {
+  return (
+    <div className="app">
+      <div className="navbar">
+        <div className="navbar-left">
+          <img src={logo} className="bus-icon" />
+          <span className="navbar-title">Bus map</span>
+        </div>
+        <Link to="/login">
+          <button className="login-btn">Đăng nhập</button>
+        </Link>
+      </div>
+
+      <div className="center-box">
+        <img src={background} className="logo" />
+      </div>
+    </div>
+  );
+}
+
