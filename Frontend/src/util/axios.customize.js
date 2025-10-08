66d691d7b4d296a9623b8b1e5750cf6a62186641
minhmh <minh.maihoang.71@gmail.com>
2025-10-08 21:47:54 +0700

    add .gitignore

--- Frontend/src/util/axios.customize.js
+++ Frontend/src/util/axios.customize.js
@@ -10,7 +10,7 @@ const instance = axios.create({
 // Add a request interceptor
 instance.interceptors.request.use(function (config) {
     // Do something before request is sent
-    
+    // add accesstoken into headers
     return config;
   }, function (error) {
     // Do something with request error
